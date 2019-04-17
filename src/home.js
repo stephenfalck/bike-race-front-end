fe895ed4563477fd2f52249c43827b5e86429fd5
Stephen Falck <stephenvfalck@gmail.com>
2019-04-17 17:03:49 +0100

    moment.js countdown implemented and home page completed

--- src/home.js
+++ src/home.js
@@ -1,28 +1,81 @@
 import React from 'react';
 import { MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import moment from 'moment';
 import './home.css'
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTime: moment().unix(),
+            months: null,
+            days: null,
+            hours: null,
+            minutes: null,
+            seconds: null
+        }
+    }
+    
+    timeRemaining() {
+        
+        const raceTime = moment('01-04-2020 09:00:00', 'DD-MM-YYYY HH:mm:ss').unix();
+        let currentTime = this.state.currentTime;
+        let timeDifference = raceTime - currentTime;
+        let duration = moment.duration(timeDifference * 1000, 'milliseconds')
+        //console.log(duration);
+        let months = moment.duration(duration).months()
+        let days = moment.duration(duration).days()
+        let hours = moment.duration(duration).hours()
+        let minutes = moment.duration(duration).minutes()
+        let seconds = moment.duration(duration).seconds()
+
+        months = months < 10 ? '0' + months : months;
+        days = days < 10 ? '0' + days : days;
+        hours = hours < 10 ? '0' + hours : hours;
+        minutes = minutes < 10 ? '0' + minutes : minutes; 
+        seconds = seconds < 10 ? '0' + seconds : seconds; 
+        
+        this.setState({ 
+            currentTime: moment().unix(),
+            months: months,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        })
+           
+    }
+    componentDidMount() {
+        this.interval = setInterval(() => this.timeRemaining(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 
     render() {
         return (
-           
                 <div className='main-background'>
                     <div className="main-overlay">
                         <MDBContainer className="main-body">
-                            
                             <MDBRow className="main-content">
-                                
                                 <MDBCol size="12" sm="6" className="order-last countdown">
-                                    <h1>Days</h1>
                                     <MDBRow>
-                                        <MDBCol size="12">Hours Minutes Seconds</MDBCol>
+                                        <MDBCol size="12">
+                                        <h1 className="font-weight-bold" id="month-unit">{this.state.months}</h1>
+                                        <h1 id="countdown-month-header">Months</h1>
+                                        </MDBCol>
+                                    </MDBRow>
+                                    <MDBRow>
+                                        <MDBCol size="3"><h2 className="units">{this.state.days}</h2><p>Days</p></MDBCol>
+                                        <MDBCol size="3"><h2 className="units">{this.state.hours}</h2> <p>Hours</p></MDBCol> 
+                                        <MDBCol size="3"><h2 className="units">{this.state.minutes}</h2> <p>Minutes</p></MDBCol> 
+                                        <MDBCol size="3"><h2 className="units">{this.state.seconds}</h2> <p>Seconds</p></MDBCol> 
                                     </MDBRow>
                                 </MDBCol>
-                                
                                 <MDBCol size="12" sm="6" className="order-md-last">
-                                    <h1>Boulder Bike Tour</h1>
-                                    <h3>All you need to know about the upcoming race</h3>
+                                    <h1 className="font-weight-bold">Boulder Bike Tour</h1>
+                                    <h3>1st April 2020</h3>
                                     <MDBRow>
                                         <MDBCol size="12">
                                         The Boulder Bike Tour is a multiple stage bicycle race
@@ -43,8 +96,6 @@ class Home extends React.Component {
                             </MDBContainer>
                         </div>
                     </div>
-                
-            
         )
     }
 
