c95222797339e25d12669d9a0b7b415f789d9437
Stephen Falck <stephenvfalck@gmail.com>
2019-04-28 12:37:50 +0100

    changed api request paths to rails back to development paths

--- src/riders/riders.js
+++ src/riders/riders.js
@@ -23,8 +23,8 @@ class Riders extends React.Component {
     }
 
     getRiders() {
-        //fetch('/riders')
-        fetch('https://ancient-cove-95094.herokuapp.com/riders')
+        fetch('/riders')
+        //fetch('https://ancient-cove-95094.herokuapp.com/riders')
         .then(res => res.json())
         .then(
             (data) => {
